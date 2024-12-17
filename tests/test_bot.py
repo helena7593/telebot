cd754eb8dbd35e3c599fa495201a7ac4858299b4
User <helena7593@gmail.com>
2024-12-17 15:37:14 +0200

    tests

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -16,7 +16,7 @@ def test_doctor_time1():
 
 def test_doctor_time2():
     dentist = Dentist()
-    assert dentist.book_time("17:00") == False
+    assert dentist.book_time("18:00") == True
 
 def test_doctor_time3():
     cardiologist = Cardiologist()
