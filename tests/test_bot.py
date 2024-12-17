89371e6291ee06052ed6e1f57f29531487488b43
User <helena7593@gmail.com>
2024-12-17 12:15:50 +0200

    tests

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -10,11 +10,23 @@ def test_doctor_time():
     assert dentist.book_time("10:00") == True
     assert "10:00" not in dentist.schedule
 
+def test_doctor_time1():
+    dentist = Dentist()
     assert dentist.book_time("10:00") == False
+
+def test_doctor_time2():
+    dentist = Dentist()
     assert dentist.book_time("13:00") == False
 
+def test_doctor_time3():
     cardiologist = Cardiologist()
     assert cardiologist.name == "Cardiologist"
+
+def test_doctor_time4():
+    cardiologist = Cardiologist()
     assert cardiologist.schedule == ["16:00", "17:00", "18:00"]
+
+def test_doctor_time5():
+    cardiologist = Cardiologist()
     assert cardiologist.book_time("17:00") == True
     assert "17:00" not in cardiologist.schedule
